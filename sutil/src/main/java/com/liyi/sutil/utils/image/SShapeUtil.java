d263c1c659e749ee853a3c6da3f76def8b49cc2e
albertlii <albertlii@163.com>
2017-08-04 10:57:10 +0800

    crashhandler

--- sutil/src/main/java/com/liyi/sutil/utils/image/SShapeUtil.java
+++ sutil/src/main/java/com/liyi/sutil/utils/image/SShapeUtil.java
@@ -4,11 +4,6 @@ import android.graphics.drawable.GradientDrawable;
 
 public class SShapeUtil {
 
-    /**
-     * @param fillColor
-     * @param radius
-     * @return
-     */
     public static GradientDrawable getRectShape(int fillColor, int radius) {
         GradientDrawable shape = new GradientDrawable();
         shape.setShape(GradientDrawable.RECTANGLE);
@@ -17,13 +12,6 @@ public class SShapeUtil {
         return shape;
     }
 
-    /**
-     * @param fillColor
-     * @param radius
-     * @param strokeColor
-     * @param strokeWidth
-     * @return
-     */
     public static GradientDrawable getRectShape(int fillColor, int radius, int strokeColor, int strokeWidth) {
         GradientDrawable shape = new GradientDrawable();
         shape.setShape(GradientDrawable.RECTANGLE);
@@ -33,13 +21,6 @@ public class SShapeUtil {
         return shape;
     }
 
-    /**
-     * @param fillColor
-     * @param radii
-     * @param strokeColor
-     * @param strokeWidth
-     * @return
-     */
     public static GradientDrawable getRectShape(int fillColor, float[] radii, int strokeColor, int strokeWidth) {
         GradientDrawable shape = new GradientDrawable();
         shape.setShape(GradientDrawable.RECTANGLE);
