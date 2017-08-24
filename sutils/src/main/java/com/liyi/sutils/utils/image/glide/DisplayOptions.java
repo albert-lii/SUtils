bcd950e1392d40de566f7743f816be6c91f1f13c
albertlii <albertlii@163.com>
2017-08-24 15:01:35 +0800

    Glide工具类  displayOptions

---
+++ sutils/src/main/java/com/liyi/sutils/utils/image/glide/DisplayOptions.java
@@ -0,0 +1,38 @@
+package com.liyi.sutils.utils.image.glide;
+
+
+import android.content.Context;
+
+import com.bumptech.glide.request.RequestOptions;
+import com.liyi.sutils.utils.image.glide.transform.CircleTransform;
+import com.liyi.sutils.utils.image.glide.transform.RoundTransform;
+import com.liyi.sutils.utils.image.glide.transform.blur.BlurTransformation;
+
+public class DisplayOptions {
+    /**
+     * 圆形
+     */
+    public static RequestOptions circleOptions(Context context) {
+        RequestOptions options = new RequestOptions();
+        options.transform(new CircleTransform(context));
+        return options;
+    }
+
+    /**
+     * 圆角
+     */
+    public static RequestOptions roundOptions(Context context, int radius) {
+        RequestOptions options = new RequestOptions();
+        options.transform(new RoundTransform(context, radius));
+        return options;
+    }
+
+    /**
+     * 高斯模糊
+     */
+    public static RequestOptions blurOptions(Context context, int radius) {
+        RequestOptions options = new RequestOptions();
+        options.transform(new BlurTransformation(context, radius));
+        return options;
+    }
+}
