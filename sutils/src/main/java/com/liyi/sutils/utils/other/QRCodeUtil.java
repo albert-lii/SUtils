1b486cdc7a656737f6a9b5288128a227c585fa63
albertlii <albertlii@163.com>
2017-09-01 16:05:53 +0800

    添加二维码相关工具类

---
+++ sutils/src/main/java/com/liyi/sutils/utils/other/QRCodeUtil.java
@@ -0,0 +1,417 @@
+package com.liyi.sutils.utils.other;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PointF;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.ChecksumException;
+import com.google.zxing.FormatException;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.PlanarYUVLuminanceSource;
+import com.google.zxing.RGBLuminanceSource;
+import com.google.zxing.ReaderException;
+import com.google.zxing.Result;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.qrcode.QRCodeReader;
+
+/**
+ * 二维码工具类
+ */
+
+public class QRCodeUtil {
+
+    /***********************************************************************************************
+     *****  生成二维码
+     **********************************************************************************************/
+
+    /**
+     * 生成二维码的Bitmap
+     *
+     * @param content 二维码中的内容
+     * @param width   二维码的宽
+     * @param height  二维码的高
+     * @return 二维码图片
+     */
+    public static Bitmap generateQRImage(@NonNull String content, int width, int height) {
+        Bitmap bitmap = null;
+        BitMatrix result = null;
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            result = multiFormatWriter.encode(content, BarcodeFormat.QR_CODE, width, height);
+            int[] pixels = new int[width * height];
+            for (int y = 0; y < height; y++) {
+                int offset = y * width;
+                for (int x = 0; x < width; x++) {
+                    pixels[offset + x] = result.get(x, y) ? Color.BLACK : Color.WHITE;
+                }
+            }
+            bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+    /**
+     * 在二维码中间添加Logo图案
+     */
+    public static Bitmap addLogo(Bitmap src, Bitmap logo) {
+        if (src == null) {
+            return null;
+        }
+        if (logo == null) {
+            return src;
+        }
+        // 获取图片的宽高
+        int srcWidth = src.getWidth();
+        int srcHeight = src.getHeight();
+        int logoWidth = logo.getWidth();
+        int logoHeight = logo.getHeight();
+        if (srcWidth == 0 || srcHeight == 0) {
+            return null;
+        }
+        if (logoWidth == 0 || logoHeight == 0) {
+            return src;
+        }
+        // logo大小为二维码整体大小的1/5
+        float scaleFactor = srcWidth * 1.0f / 5 / logoWidth;
+        Bitmap bitmap = Bitmap.createBitmap(srcWidth, srcHeight, Bitmap.Config.ARGB_8888);
+        try {
+            Canvas canvas = new Canvas(bitmap);
+            canvas.drawBitmap(src, 0, 0, null);
+            canvas.scale(scaleFactor, scaleFactor, srcWidth / 2, srcHeight / 2);
+            canvas.drawBitmap(logo, (srcWidth - logoWidth) / 2, (srcHeight - logoHeight) / 2, null);
+            canvas.save(Canvas.ALL_SAVE_FLAG);
+            canvas.restore();
+        } catch (Exception e) {
+            bitmap = null;
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+    /***********************************************************************************************
+     ****  解析二维码
+     **********************************************************************************************/
+
+    /**
+     * 解析二维码（使用解析RGB编码数据的方式）
+     *
+     * @param path
+     * @return
+     */
+    public static Result decodeQRcodeRGB(String path) {
+        if (TextUtils.isEmpty(path)) {
+            return null;
+        }
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inSampleSize = 1;
+        Bitmap qrcode = BitmapFactory.decodeFile(path, opts);
+        Result result = decodeQRcodeRGB(qrcode);
+        qrcode.recycle();
+        qrcode = null;
+        return result;
+    }
+
+    /**
+     * 解析二维码 （使用解析RGB编码数据的方式）
+     *
+     * @param qrcode
+     * @return
+     */
+    public static Result decodeQRcodeRGB(Bitmap qrcode) {
+        int width = qrcode.getWidth();
+        int height = qrcode.getHeight();
+        int[] data = new int[width * height];
+        qrcode.getPixels(data, 0, width, 0, 0, width, height);
+        RGBLuminanceSource source = new RGBLuminanceSource(width, height, data);
+        BinaryBitmap bitmap1 = new BinaryBitmap(new HybridBinarizer(source));
+        QRCodeReader reader = new QRCodeReader();
+        Result result = null;
+        try {
+            result = reader.decode(bitmap1);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        } catch (ChecksumException e) {
+            e.printStackTrace();
+        } catch (FormatException e) {
+            e.printStackTrace();
+        }
+        qrcode.recycle();
+        qrcode = null;
+        return result;
+    }
+
+    /**
+     * 解析二维码（使用解析YUV编码数据的方式）
+     *
+     * @param path
+     * @return
+     */
+    public static Result decodeQRcodeYUV(String path) {
+        if (TextUtils.isEmpty(path)) {
+            return null;
+        }
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inSampleSize = 1;
+        Bitmap barcode = BitmapFactory.decodeFile(path, opts);
+        Result result = decodeQRcodeYUV(barcode);
+        barcode.recycle();
+        barcode = null;
+        return result;
+    }
+
+    /**
+     * 解析二维码（使用解析YUV编码数据的方式）
+     *
+     * @param qrcode
+     * @return
+     */
+    public static Result decodeQRcodeYUV(Bitmap qrcode) {
+        if (null == qrcode) {
+            return null;
+        }
+        int width = qrcode.getWidth();
+        int height = qrcode.getHeight();
+        // 以argb方式存放图片的像素
+        int[] argb = new int[width * height];
+        qrcode.getPixels(argb, 0, width, 0, 0, width, height);
+        // 将argb转换为yuv
+        byte[] yuv = new byte[width * height * 3 / 2];
+        encodeYUV420SP(yuv, argb, width, height);
+        // 解析YUV编码方式的二维码
+        Result result = decodeQRcodeYUV(yuv, width, height);
+
+        qrcode.recycle();
+        qrcode = null;
+        return result;
+    }
+
+    /**
+     * 解析二维码（使用解析YUV编码数据的方式）
+     *
+     * @param yuv
+     * @param width
+     * @param height
+     * @return
+     */
+    private static Result decodeQRcodeYUV(byte[] yuv, int width, int height) {
+        long start = System.currentTimeMillis();
+        MultiFormatReader multiFormatReader = new MultiFormatReader();
+        multiFormatReader.setHints(null);
+
+        Result rawResult = null;
+        PlanarYUVLuminanceSource source = new PlanarYUVLuminanceSource(yuv, width, height, 0, 0,
+                width, height, false);
+        if (source != null) {
+            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+            try {
+                rawResult = multiFormatReader.decodeWithState(bitmap);
+            } catch (ReaderException re) {
+                re.printStackTrace();
+            } finally {
+                multiFormatReader.reset();
+                multiFormatReader = null;
+            }
+        }
+        return rawResult;
+    }
+
+    /**
+     * RGB 转 YUV 的公式是:
+     * Y=0.299R + 0.587G + 0.114B;
+     * U=-0.147R - 0.289G + 0.436B;
+     * V=0.615R - 0.515G - 0.1B;
+     *
+     * @param yuv
+     * @param argb
+     * @param width
+     * @param height
+     */
+    private static void encodeYUV420SP(byte[] yuv, int[] argb, int width, int height) {
+        // 帧图片的像素大小
+        final int frameSize = width * height;
+        // ---YUV数据---
+        int Y, U, V;
+        // Y的index从0开始
+        int yIndex = 0;
+        // UV的index从frameSize开始
+        int uvIndex = frameSize;
+        // ---颜色数据---
+        int R, G, B;
+        int rgbIndex = 0;
+        // ---循环所有像素点，RGB转YUV---
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                R = (argb[rgbIndex] & 0xff0000) >> 16;
+                G = (argb[rgbIndex] & 0xff00) >> 8;
+                B = (argb[rgbIndex] & 0xff);
+                //
+                rgbIndex++;
+                // well known RGB to YUV algorithm
+                Y = ((66 * R + 129 * G + 25 * B + 128) >> 8) + 16;
+                U = ((-38 * R - 74 * G + 112 * B + 128) >> 8) + 128;
+                V = ((112 * R - 94 * G - 18 * B + 128) >> 8) + 128;
+                Y = Math.max(0, Math.min(Y, 255));
+                U = Math.max(0, Math.min(U, 255));
+                V = Math.max(0, Math.min(V, 255));
+                // NV21 has a plane of Y and interleaved planes of VU each sampled by a factor of 2
+                // meaning for every 4 Y pixels there are 1 V and 1 U. Note the sampling is every other
+                // pixel AND every other scan line.
+                // ---Y---
+                yuv[yIndex++] = (byte) Y;
+                // ---UV---
+                if ((j % 2 == 0) && (i % 2 == 0)) {
+                    //
+                    yuv[uvIndex++] = (byte) V;
+                    //
+                    yuv[uvIndex++] = (byte) U;
+                }
+            }
+        }
+    }
+
+
+    /***********************************************************************************************
+     *****  条形码相关
+     **********************************************************************************************/
+
+    /**
+     * 生成条形码
+     *
+     * @param context
+     * @param contents      需要生成的内容
+     * @param desiredWidth  生成条形码的宽带
+     * @param desiredHeight 生成条形码的高度
+     * @param displayCode   是否在条形码下方显示内容
+     * @return
+     */
+    public static Bitmap generateBarImage(Context context, String contents, int desiredWidth, int desiredHeight, boolean displayCode) {
+        Bitmap ruseltBitmap = null;
+        /**
+         * 图片两端所保留的空白的宽度
+         */
+        int marginW = 20;
+        /**
+         * 条形码的编码类型
+         */
+        BarcodeFormat barcodeFormat = BarcodeFormat.CODE_128;
+        if (displayCode) {
+            Bitmap barcodeBitmap = encodeAsBitmap(contents, barcodeFormat,
+                    desiredWidth, desiredHeight);
+            Bitmap codeBitmap = createCodeBitmap(contents, desiredWidth + 2
+                    * marginW, desiredHeight, context);
+            ruseltBitmap = mixtureBitmap(barcodeBitmap, codeBitmap, new PointF(
+                    0, desiredHeight));
+        } else {
+            ruseltBitmap = encodeAsBitmap(contents, barcodeFormat,
+                    desiredWidth, desiredHeight);
+        }
+        return ruseltBitmap;
+    }
+
+    /**
+     * 生成条形码的Bitmap
+     *
+     * @param contents      需要生成的内容
+     * @param format        编码格式
+     * @param desiredWidth
+     * @param desiredHeight
+     * @return
+     * @throws WriterException
+     */
+    private static Bitmap encodeAsBitmap(String contents, BarcodeFormat format, int desiredWidth, int desiredHeight) {
+        MultiFormatWriter writer = new MultiFormatWriter();
+        BitMatrix result = null;
+        try {
+            result = writer.encode(contents, format, desiredWidth, desiredHeight, null);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        int width = result.getWidth();
+        int height = result.getHeight();
+        int[] pixels = new int[width * height];
+        // All are 0, or black, by default
+        for (int y = 0; y < height; y++) {
+            int offset = y * width;
+            for (int x = 0; x < width; x++) {
+                pixels[offset + x] = result.get(x, y) ? Color.BLACK : Color.WHITE;
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(width, height,
+                Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        return bitmap;
+    }
+
+    /**
+     * 生成显示编码的Bitmap
+     *
+     * @param contents
+     * @param width
+     * @param height
+     * @param context
+     * @return
+     */
+    private static Bitmap createCodeBitmap(String contents, int width, int height, Context context) {
+        TextView tv = new TextView(context);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        tv.setLayoutParams(layoutParams);
+        tv.setText(contents);
+        tv.setHeight(height);
+        tv.setGravity(Gravity.CENTER_HORIZONTAL);
+        tv.setWidth(width);
+        tv.setDrawingCacheEnabled(true);
+        tv.setTextColor(Color.BLACK);
+        tv.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        tv.layout(0, 0, tv.getMeasuredWidth(), tv.getMeasuredHeight());
+        tv.buildDrawingCache();
+        Bitmap bitmapCode = tv.getDrawingCache();
+        return bitmapCode;
+    }
+
+    /**
+     * 将两个Bitmap合并成一个
+     *
+     * @param first
+     * @param second
+     * @param fromPoint 第二个Bitmap开始绘制的起始位置（相对于第一个Bitmap）
+     * @return
+     */
+    private static Bitmap mixtureBitmap(Bitmap first, Bitmap second, PointF fromPoint) {
+        if (first == null || second == null || fromPoint == null) {
+            return null;
+        }
+        int marginW = 20;
+        Bitmap newBitmap = Bitmap.createBitmap(
+                first.getWidth() + second.getWidth() + marginW,
+                first.getHeight() + second.getHeight(), Bitmap.Config.ARGB_4444);
+        Canvas cv = new Canvas(newBitmap);
+        cv.drawBitmap(first, marginW, 0, null);
+        cv.drawBitmap(second, fromPoint.x, fromPoint.y, null);
+        cv.save(Canvas.ALL_SAVE_FLAG);
+        cv.restore();
+        return newBitmap;
+    }
+}
