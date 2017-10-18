4715028d1ab01cba1b33eb78eaff929660ed0177
Yi Li <albertlii@163.com>
2017-10-18 18:19:58 +0800

    Update OnParseResultListener.java

--- sutils/src/main/java/com/liyi/sutils/utils/network/nohttp/callback/OnParseResultListener.java
+++ sutils/src/main/java/com/liyi/sutils/utils/network/nohttp/callback/OnParseResultListener.java
@@ -4,8 +4,9 @@ package com.liyi.sutils.utils.network.nohttp.callback;
 /**
  * 解析nohttp网络请求返回的数据
  *
- * @param <T> nohttp网络请求返回的数据的类型
+ * @param <K> nohttp网络请求返回的数据的类型
+ * @param <V> 解析网络请求数据后返回的结果
  */
-public interface OnParseResultListener<K,T> {
-    K onParseResult(int what, T t);
+public interface OnParseResultListener<K,V> {
+    V onParseResult(int what, K k);
 }
